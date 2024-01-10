3a1241a92496b51c430d0cb413f1619e560e697b
lukonianlogic <lucas.longacre@gmail.com>
2024-01-10 12:41:22 -0700

    Helmet & SEO Added
    SEO for main page added to site.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
 import Testimonials from "./Components/Testimonials";
+import { Helmet } from "react-helmet";
 
 class App extends Component {
   constructor(props) {
@@ -28,9 +29,9 @@ class App extends Component {
       url: "./resumeData.json",
       dataType: "json",
       cache: false,
-      success: function(data) {
+      success: (data) => {
         this.setState({ resumeData: data });
-      }.bind(this),
+      },      
       error: function(xhr, status, err) {
         console.log(err);
         alert(err);
@@ -43,9 +44,9 @@ class App extends Component {
       url: "./testimonialData.json",
       dataType: "json",
       cache: false,
-      success: function (data) {
+      success: (data) => {
         this.setState({ testimonialData: data });
-      }.bind(this),
+      },      
       error: function (xhr, status, err) {
         console.log(err);
         alert(err);
@@ -61,6 +62,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+               <Helmet>
+                <title>Lucas Longacre - AI Consultant, Product Manager, Full Stack Developer</title>
+                <meta
+                  name="description"
+                  content="Experienced AI consultant, product manager, and full stack developer. Crafting innovative solutions and driving business success in AI, product management, and software development. Explore my portfolio and resume."
+                />
+                <meta
+                  name="keywords"
+                  content="AI consultant, product manager, full stack developer, innovative solutions, business success, AI, product management, software development"
+                />
+                <meta name="author" content="Lucas Longacre" />
+              </Helmet>
+
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main} />
         <Portfolio data={this.state.resumeData.portfolio} />
